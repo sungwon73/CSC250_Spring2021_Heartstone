ef74a4b16a2cb17151ded81929b6572f526929c5
sungwon73 <78753573+sungwon73@users.noreply.github.com>
2021-02-23 02:09:56 +0900

    intial commit

--- src/Heartstonecard.java
+++ src/Heartstonecard.java
@@ -1,4 +1,31 @@
 
-public class Heartstonecard {
-
+public class HearthstoneCard 
+{
+	private int cost;
+	private int attack;
+	private int defense;
+	private String name;
+	
+	public HearthstoneCard(String name, int cost, int attack, int defense)
+	{
+		this.cost = cost;
+		this.attack = attack;
+		this.defense = defense;
+		this.name = name;
+	}
+	
+	//setters allow us to conditionally change the value of a private member
+	public void setName(String name)
+	{
+		if(name.length() >= 5)
+		{
+			this.name = name;
+		}
+	}
+	
+	void display()
+	{
+		//System.out.println("Name: " + this.name + "\nCost" + this.cost + "\nAttack: " + this.attack + " Defense: " + this.defense);
+		System.out.format("Name: %s \nCost: %d \nAttack: %d Defense: %d\n", this.name, this.cost, this.attack,this.defense);
+	}
 }
